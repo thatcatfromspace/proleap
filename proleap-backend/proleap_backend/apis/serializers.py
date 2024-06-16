dc8964aea9b9579ddd67472628568939d73e69e5
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-06-17 03:00:26 +0530

    Add UserBatch Modify Logic

--- proleap-backend/proleap_backend/apis/serializers.py
+++ proleap-backend/proleap_backend/apis/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import ValidationError
 
 
-from .models import (User, Batch, UserBatch)
+from .models import (User, Batch, UserBatch, Status)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,9 +26,44 @@ class BatchSerializer(serializers.ModelSerializer):
 
 
 class UserBatchSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    batch = BatchSerializer(read_only=True)
+    user_id = serializers.IntegerField()
+    batch_id = serializers.IntegerField()
 
     class Meta:
         model = UserBatch
-        fields = '__all__'
+        fields = ['id', 'user_id', 'batch_id', 'completed_activities', 'is_completed', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+
+    def create(self, validated_data):
+        user_id = validated_data.pop('user_id')
+        batch_id = validated_data.pop('batch_id')
+
+        try:
+            user_batch, created = UserBatch.objects.update_or_create(
+                user_id=user_id,
+                batch_id=batch_id,
+                defaults={
+                    'completed_activities': validated_data.get('completed_activities', 0),
+                    'is_completed': validated_data.get('is_completed', False),
+                    'status': validated_data.get('status', Status.NOT_ATTEMPTED)
+                }
+            )
+            return user_batch
+        except Exception as e:
+            raise serializers.ValidationError(str(e))
+        
+    def update(self, instance, validated_data):
+        # Exclude user_id and batch_id from validated_data to keep them immutable
+        validated_data.pop('user_id', None)
+        validated_data.pop('batch_id', None)
+
+        instance.completed_activities = validated_data.get('completed_activities', instance.completed_activities)
+        instance.is_completed = validated_data.get('is_completed', instance.is_completed)
+        instance.status = validated_data.get('status', instance.status)
+
+        try:
+            instance.save()
+            return instance
+        except Exception as e:
+            raise serializers.ValidationError(str(e))
