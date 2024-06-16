bfe53d2dacd2767c0ecd1a7863187f64354f6907
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-06-17 01:33:49 +0530

    Add batch userbatch serializer

--- proleap-backend/proleap_backend/apis/serializers.py
+++ proleap-backend/proleap_backend/apis/serializers.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import authenticate
 
 
-from .models import User
+from .models import (User, Batch, UserBatch)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +14,19 @@ class UserSerializer(serializers.ModelSerializer):
                 'write_only': True
             }
         }
+
+
+class BatchSerializer(serializers.ModelSerializer):
+    # users = UserSerializer(many=True, read_only=True, source='users_set')
+    class Meta:
+        model = Batch
+        fields = '__all__'
+
+
+class UserBatchSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    batch = BatchSerializer(read_only=True)
+
+    class Meta:
+        model = UserBatch
+        fields = '__all__'
