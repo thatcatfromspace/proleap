d79eecac52f980029dddff98233c4f1e96db96fa
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-06-17 11:15:37 +0530

    Register new models

--- proleap-backend/proleap_backend/apis/admin.py
+++ proleap-backend/proleap_backend/apis/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from .models import User 
+from .models import (
+    User, 
+    Batch, UserBatch, 
+    Activity, UserActivity,
+)
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Batch)
+admin.site.register(UserBatch)
+admin.site.register(Activity)
+admin.site.register(UserActivity)
