fdd82fbbd63d3287ac9540a6f4990927b303a3a8
Prem Dharshan <whizzkid.dpd@gmail.com>
2024-06-23 19:12:35 +0530

    Add batch factory

--- proleap-backend/proleap_backend/apis/factories.py
+++ proleap-backend/proleap_backend/apis/factories.py
@@ -1,29 +1,66 @@
-import factory
-from factory.faker import faker
-from django.contrib.auth.hashers import make_password
-from .models import User, Role
+from random import randint
+from factory.django import DjangoModelFactory
+from factory import LazyFunction, LazyAttribute, SubFactory, post_generation
+from factory.faker import Faker
+from .models import Status, User, Role, Batch, UserBatch
+from datetime import datetime, timedelta
 
-
-class UserFactory(factory.django.DjangoModelFactory):
+class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
 
-    email = factory.Faker("email")
-    username = factory.Faker("user_name")
-    name = factory.Faker("name")
-    role = factory.Faker("random_element", elements=[Role.USER])
-    gender = factory.Faker("random_element", elements=['Male', 'Female', 'Other'])
-    phoneNumber = factory.Faker("numerify", text="##########")
+    email = Faker("email")
+    username = Faker("user_name")
+    name = Faker("name")
+    role = Faker("random_element", elements=[Role.USER])
+    gender = Faker("random_element", elements=['Male', 'Female', 'Other'])
+    phoneNumber = Faker("numerify", text="##########")
 
     is_active = True
     is_verified = False
     is_staff = False
 
-    created_at = factory.Faker('date_time_this_year')
-    updated_at = factory.Faker('date_time_this_year')
+    created_at = Faker('date_time_this_year')
+    updated_at = Faker('date_time_this_year')
 
-    @factory.post_generation
+    @post_generation
     def password(self, create, extracted, **kwargs):
         self.set_password('proleap')
         if extracted:
             self.set_password(extracted)
+
+
+class BatchFactory(DjangoModelFactory):
+    class Meta:
+        model = Batch
+
+    name = Faker('company')
+    year = LazyFunction(lambda: datetime.now().year)
+    start_time = Faker('date_time_this_year')
+    end_time = LazyAttribute(lambda obj: obj.start_time + timedelta(days=randint(1, 3)))
+    total_activities = LazyAttribute(2)
+
+    created_at = Faker('date_time_this_year')
+    updated_at = Faker('date_time_this_year')
+
+    is_active = True
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        organizer = UserFactory(role=Role.ORGANIZER)
+        kwargs['organizer'] = organizer
+        return super()._create(model_class, *args, **kwargs)
+
+
+class UserBatchFactory(DjangoModelFactory):
+    class Meta:
+        model = UserBatch
+
+    user = SubFactory(UserFactory) 
+    batch = SubFactory(BatchFactory)
+
+    completed_activities = 0
+    status = Faker('random_element', elements=[Status.NOT_ATTEMPTED])
+
+    created_at = Faker('date_time_this_year')
+    updated_at = Faker('date_time_this_year')
